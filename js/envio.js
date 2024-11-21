c673c05fdd11f523a207f4ddf2790e2407113e12
AngelGo26 <angelleopardo2005@gmail.com>
2024-11-21 17:01:31 +0100

    Update envio.js

--- js/envio.js
+++ js/envio.js
@@ -1,9 +1,12 @@
-document.getElementById("miniformulario").addEventListener("submit", function (event) {
-    event.preventDefault(); // Evita el envío predeterminado
-    alert("Formulario enviado con éxito.");
-    window.location.href = "index.html"; // Redirige a index.html
-});
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("miniformulario").addEventListener("submit", function (event) {
+        event.preventDefault(); // Evita el envío predeterminado
+        alert("Formulario enviado con éxito.");
+        window.location.href = "index.html"; // Redirige a index.html
+    });
 
-function resetearDatos() {
-    document.forms['miniformulario'].reset();
-}
+    // Función para reiniciar los datos
+    window.resetearDatos = function () {
+        document.getElementById("miniformulario").reset();
+    };
+});
